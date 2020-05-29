2969bc0f851dc8449ad30c508a648b0a29d61029
AjayChitumalla <47892109+AjayChitumalla@users.noreply.github.com>
2020-05-29 14:51:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist/medical'));
+app.use(express.static(__dirname+'/dist/demo'));
 
 app.listen((process.env.PORT || 4200),(req,res)=>{
     console.log(`listening at port`);
 });
 
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/dist/medical/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/demo/index.html'));
 })
